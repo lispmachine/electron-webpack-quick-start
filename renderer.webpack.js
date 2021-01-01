feac52e1dd5dfa3579e1f664d6c0f2f07c63cdca
Łukasz Cieśnik <lukasz.ciesnik@gmail.com>
2021-01-01 23:10:18 +0100

    Workaround

---
+++ renderer.webpack.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    alias: {
+      'material-ui': path.resolve('./node_modules/@material-ui'),
+    }
+  }
+};
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from 'material-ui/core';
 
 const useStyles = makeStyles({
   root: {
